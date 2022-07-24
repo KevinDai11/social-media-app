1f204cf70d78dfa220c4c3af283545e893a5f64d
Kevin Dai <kevin923.dai@gmail.com>
2022-07-24 17:55:31 -0500

    delete comments

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const {UserInputError} = require('apollo-server');
+const {AuthenticationError, UserInputError} = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check_auth');
@@ -33,5 +33,30 @@ module.exports = {
             return post;
         },
 
+
+        async deleteComment(_, {postId, commentId}, context) {
+
+            const {username} = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if(!post){
+                throw new UserInputError('Post not found');
+            }
+
+            const commentIndex = post.comments.findIndex(comment => comment.id === commentId);
+
+            if(commentIndex < 0){
+                throw new UserInputError('Comment not found');
+            }
+
+            if(post.comments[commentIndex].username !== username){
+                throw new AuthenticationError('Not authorized');
+            }
+
+            post.comments.splice(commentIndex, 1);
+            await post.save()
+            return post;
+        },
     }
 }
