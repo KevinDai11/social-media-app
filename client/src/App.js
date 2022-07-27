b44379fea81d2cf394c91d412d92fb299baada27
Kevin Dai <kevin923.dai@gmail.com>
2022-07-26 23:29:54 -0500

    added semantics and set up Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import React from 'react';
+
+import './semantics/semantic.css';
+import './semantics/semantic.min.css';
+import './App.css';
+
+import Home from './pages/Home';
 function App() {
   return (
-    <div><h1>Testpebnis</h1></div>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Login" element={< Login/>} />
+          <Route exact path = "/Register" element = {< Register/>}/>
+        </Routes>
+      </Router>
+
+    </div>
   );
 }
 
