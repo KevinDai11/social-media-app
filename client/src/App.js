b2eb2c7eb6ac313db22533a82e2ef4b0a62b8c54
Kevin Dai <kevin923.dai@gmail.com>
2022-07-28 22:05:13 -0500

    finish setting up routes, added MenuBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import React from 'react';
+import {Container} from 'semantic-ui-react';
 
 import './semantics/semantic.css';
 import './semantics/semantic.min.css';
 import './App.css';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
 function App() {
   return (
     <div>
       <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/Login" element={< Login/>} />
-          <Route exact path = "/Register" element = {< Register/>}/>
-        </Routes>
+        <Container>
+
+          <MenuBar />
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/Login" element={<Login/>} />
+            <Route exact path = "/Register" element = {<Register/>}/>
+          </Routes>
+           
+        </Container>
       </Router>
 
     </div>
