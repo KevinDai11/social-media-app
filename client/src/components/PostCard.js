feef022b2ae39d1609b63d527bf4c79f87bdb86c
Kevin Dai <kevin923.dai@gmail.com>
2022-08-01 21:58:31 -0500

    add delete post button

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Card, Image, Button} from 'semantic-ui-react';
 import moment from 'moment';
 import {Link} from 'react-router-dom';
 
+import {AuthContext} from '../context/auth';
+
 export default function PostCard({post : {id, body, createdAt, username, likeCount, likes, commentCount, comments}}) {
+
+
+    const {user} = useContext(AuthContext);
 
     function likePost(){
 
     }
-    function commentPost(){
 
-    }
     return(
         <Card fluid>
             <Card.Content as ={Link} to ={`/posts/${id}`}>
@@ -34,12 +37,27 @@ export default function PostCard({post : {id, body, createdAt, username, likeCou
                     label={{ basic: true, color: 'red', pointing: 'left', content: `${likeCount}` }}
                 />
                 <Button
-                    onClick={commentPost}
+                    as = {Link}
+                    to = {`/posts/${id}`}
                     color='teal'
                     icon = {`${commentCount}` > 1 ? 'comments' : 'comment'}
                     basic = {`${commentCount}` === 0}
                     label={{ basic: true, color: 'teal', pointing: 'left', content: `${commentCount}` }}
                 />
+                {user && user.username === username && (
+                    <Button 
+                        floated='right'
+                        color='red'
+                        onClick={
+                            () => {
+                                console.log('delete post');
+                            }
+                        }
+                        icon='trash'
+                        basic = {false}
+                    />
+                )}
+
             </Card.Content>
         </Card>
 
